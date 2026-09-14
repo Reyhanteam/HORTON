b7f23cb26ccff1f1c875ea0eed76b14f8580ce11
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-14 18:23:43 +0330

    refactor: move referral account ownership to telegram accounts

--- app/Models/ReferralAccount.php
+++ app/Models/ReferralAccount.php
@@ -6,8 +6,8 @@ class ReferralAccount extends HortonModel
 {
     protected $casts = ['commission_rate' => 'decimal:2', 'cashback_rate' => 'decimal:2', 'is_active' => 'boolean'];
 
-    public function user()
+    public function telegramAccount()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(TelegramAccount::class, 'telegram_account_id');
     }
 }
