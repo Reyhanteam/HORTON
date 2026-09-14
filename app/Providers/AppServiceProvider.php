964d37140c1ab49a291413d215f61713a1470932
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-14 19:28:27 +0330

    feat(auth): register bot settings policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\BotSetting;
 use App\Models\User;
+use App\Policies\BotSettingPolicy;
 use App\Policies\UserPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -17,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(User::class, UserPolicy::class);
+        Gate::policy(BotSetting::class, BotSettingPolicy::class);
 
         Gate::before(function (User $user): ?bool {
             if ($user->hasRole('super-admin')) {
