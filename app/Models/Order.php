d088c77d846779b39500e19d46515dd4f1df3007
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-14 13:56:07 +0330

    feat(models): add order model

---
+++ app/Models/Order.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+class Order extends HortonModel
+{
+    protected $casts = ['subtotal' => 'integer', 'discount_amount' => 'integer', 'total' => 'integer', 'metadata' => 'array', 'paid_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function invoice()
+    {
+        return $this->hasOne(Invoice::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
+
+    public function discountUsages()
+    {
+        return $this->hasMany(DiscountUsage::class);
+    }
+}
