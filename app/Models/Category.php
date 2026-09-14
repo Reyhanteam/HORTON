fa6876da14212d102be56c7b2ee9095f38100c06
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-14 13:55:44 +0330

    feat(models): add category model

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+class Category extends HortonModel
+{
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
