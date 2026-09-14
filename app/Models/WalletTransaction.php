0a26fd4d2a777e6c701a83cf2902212c479adb52
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-14 13:55:40 +0330

    feat(models): add wallet transaction model

---
+++ app/Models/WalletTransaction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+class WalletTransaction extends HortonModel
+{
+    protected $casts = ['amount' => 'integer', 'balance_before' => 'integer', 'balance_after' => 'integer', 'metadata' => 'array', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+
+    public function wallet()
+    {
+        return $this->belongsTo(Wallet::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
