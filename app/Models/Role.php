9846023f269e105723ac213177e27d95133434b0
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-15 16:25:33 +0330

    fix(auth): preserve users role pivot mapping

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,6 +11,6 @@ class Role extends HortonModel
 
     public function users()
     {
-        return $this->belongsToMany(User::class, 'role_user', 'user_id', 'role_id');
+        return $this->belongsToMany(User::class, 'role_user', 'role_id', 'user_id');
     }
 }
