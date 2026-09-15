9dce2689096ee0451d705bc82a3e4c8f09dd65eb
REYHANTEAM <hosseinakbarii3776@gmail.com>
2026-09-15 16:25:50 +0330

    refactor(auth): keep users-only auth configuration

--- config/auth.php
+++ config/auth.php
@@ -83,7 +83,8 @@ return [
     | and the user provider that is invoked to actually retrieve users.
     |
     | The expiry time is the number of minutes that each reset token will be
-    | considered valid. You may change this as needed.
+    | considered valid. This security feature keeps tokens short-lived so
+    | they have less time to be guessed. You may change this as needed.
     |
     | The throttle setting is the number of seconds a user must wait before
     | generating more password reset tokens. This prevents the user from
